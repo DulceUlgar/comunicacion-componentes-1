90a94fa338a876c93de4a90e0ae9a6bb3eed4ddf
DulceUlgar <133334486+DulceUlgar@users.noreply.github.com>
2023-06-19 13:06:06 +0200

    changes ts

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-formulario',
-  templateUrl: './formulario.component.html',
-  styleUrls: ['./formulario.component.scss']
+  selector: 'app-lista',
+  template: `
+    <h2>Lista de Personas:</h2>
+    <ul>
+      <li *ngFor="let persona of personas">
+        ID: {{ persona.id }},
+        Nombre: {{ persona.name }},
+        Apellido: {{ persona.lastName }},
+        Cargo: {{ persona.position }},
+        Salario: {{ persona.salary }}
+      </li>
+    </ul>
+  `
 })
-export class FormularioComponent {
+export class ListaComponent {
+  personas: any[] = [];
 
+  agregarPersona(persona: any) {
+    this.personas.push(persona);
+  }
 }
